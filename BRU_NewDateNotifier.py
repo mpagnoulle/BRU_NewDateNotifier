90d6f00a35684718f678f25183da685d846c2d6a
Maxime Pagnoulle <mpagnoulle@gmail.com>
2022-07-20 19:19:43 -0400

    Update BRU_NewDateNotifier.py

--- BRU_NewDateNotifier.py
+++ BRU_NewDateNotifier.py
@@ -6,6 +6,7 @@ from datetime import datetime
 # Calendar related
 calendar_url = "https://webcalendar.brucity.be/qmaticwebbooking/rest/schedule/branches/e58833ee8321437b292cd75df53f283fdc0b56ca09b678971ad3ca509edcb862/dates;servicePublicId=hidebfeab6987796c5f484529fcea02e924401747b1aca9d20a3695623027cc1;customSlotLength=35"
 target_date = datetime(2022, 8, 16)
+earliest_date = target_date
 
 # Email related
 receiver_email = ""
@@ -24,7 +25,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 # Fetching new dates and acting if needed
-def fetch_date(sc):
+def fetch_date():
     try:
         print(">>> Fetching new date...")
         req = requests.get(calendar_url)
@@ -32,15 +33,19 @@ def fetch_date(sc):
             data = req.json()
             fetched_date = datetime.strptime(data[0]["date"], '%Y-%m-%d')
             fetched_date_str = fetched_date.strftime('%Y-%m-%d')
-            print(f"    Earliest date is {fetched_date_str}")
-            if(fetched_date < target_date):
+            target_date_str = target_date.strftime('%Y-%m-%d')
+            earliest_date_str = earliest_date.strftime('%Y-%m-%d')
+            print(f"    Target date: {target_date_str}")
+            print(f"    Earliest date found: {earliest_date_str}")
+            print(f"    Current earliest date: {fetched_date_str}\n")
+            if(fetched_date < target_date and fetched_date < earliest_date):
                 send_email_notification(fetched_date_str)
             else:
-                print(f"    \033[93mSadly not earlier :(\033[0m\n")
+                print(f"    \033[93mNothing yet :(\033[0m\n")
     except Exception as e:
         logger.error(traceback.format_exc())
     finally:
-        sc.enter(11400, 1, fetch_date, (sc,))
+        print(f"    [DONE]\n")
 
 # Email Sender
 def send_email_notification(new_date):
@@ -66,8 +71,15 @@ def send_email_notification(new_date):
         server.login(smtp_email, smtp_password)
         server.sendmail(msg['From'], [msg['To']], msg.as_string())
 
+    global earliest_date
+    earliest_date = datetime.strptime(new_date, '%Y-%m-%d')
     logger.info(f"Earlier date found ({new_date}), email was sent!")
     print(f"    Email sent!\033[0m\n")
+
+def reset_earliest_date():
+    global earliest_date
+    earliest_date = target_date
+    print(">>> Earliest date has been reset!")
 
 # Ask for password
 if not smtp_password:
@@ -76,6 +88,10 @@ os.system('clear')
 
 # Start checking for dates regularly
 print("New dates will be checked for around every 10 minutes...")
-s = sched.scheduler(time.time, time.sleep)
-s.enter(1, 1, fetch_date, (s,))
-s.run()
+fetch_date()
+schedule.every(10).minutes.do(fetch_date)
+schedule.every().day.at("00:00").do(reset_earliest_date)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
